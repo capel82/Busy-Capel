c6e67585f69751e3846d651be152a0d8651c7a8c
capel82 <sandoralai@gmail.com>
2020-01-31 09:52:41 +0000

    adding gitignore and templates folder

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMnogo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
