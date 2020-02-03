5c2e36cae441cdfe2667977680a54ecc09bac7e9
capel82 <sandoralai@gmail.com>
2020-02-03 12:09:13 +0000

    corrected mongoDB url

--- run.py
+++ run.py
@@ -1,20 +1,27 @@
 import os
-from flask import Flask
-from flask_pymongo import PyMnogo
-from bson.objectid import ObjectId
+
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+# Just adding the import env for you
+import env
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_DBNAME"] = 'Cluster0'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
-def hello():
-    return 'Hello World ...today'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=int(8000),
             debug=True)
