ea47c16a2dc42a8b4a7922b4ca3500fc7390d6ec
capel82 <sandoralai@gmail.com>
2020-02-27 12:52:36 +0000

    created index.html but without functioning links

--- run.py
+++ run.py
@@ -4,22 +4,21 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
-# Just adding the import env for you
 import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'Cluster0'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
-
 mongo = PyMongo(app)
 
-
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("index.html", tasks=mongo.db.tasks.find())
-
+@app.route('/homepage')
+def homepage():
+    return render_template("index.html", recipes=mongo.db.recipes.find().limit(4))
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find().limit(3))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
