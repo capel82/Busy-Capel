9f83918a739d1611e8904b05cf996e7602d7966f
capel82 <sandoralai@gmail.com>
2020-03-03 15:19:57 +0000

    draft register page

--- run.py
+++ run.py
@@ -3,20 +3,28 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+#---- forms extension-flask WTF----#
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from forms import RegistrationForm
+#---- secret keys for MongoDB Atlas----#
 import env
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'Cluster0'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 mongo = PyMongo(app)
 
 @app.route('/')
 
 @app.route('/homepage')
 def homepage():
-    return render_template("index.html", recipes=mongo.db.recipes.find().limit(4))
+    return render_template("index.html", title='Homepage', recipes=mongo.db.recipes.find().limit(4))
+
 # ----- READ ALL RECIPES ----- #
 @app.route('/allrecipes')
 def allrecipes():
@@ -25,6 +33,25 @@ def allrecipes():
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find().limit(3))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('homepage'))
+    return render_template('register.html', title='register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='login', form=form)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
